1efe9fad051edbe13d737f58ef44afab0f699cfe
Alexsandro Monteiro de Oliveira <alexsandro.oliveira@Alexsandros-MacBook-Air.local>
2023-03-01 21:24:26 -0300

    update

--- my_project/src/components/Form.js
+++ my_project/src/components/Form.js
@@ -1,22 +1,42 @@
+
+import { useState } from 'react'
+
 function Form({number}){
 
-    function registerUsere(){
+    function registerUsere(e){
+        e.preventDefault()
         console.log('gone!')
-        alert('user regited!')
+        console.log(name, password) 
+        //alert('user regited!')
     }
 
-    // function registerUsere(e){
-    //     e.preventDefault()
-    //     console.log('gone!')
-    //     //alert('user regited!')
-    // }
+    const [name, setName] = useState('Alex')
+    const [password, setPassword] = useState()
 
     return (
         <>
         <h1>My Register</h1>
         <form onSubmit={registerUsere}>
             <div>
-                <input type="text" placeholder="write your text"/>
+                <label htmlFor="name">Nome: </label>
+                <input 
+                type="text"
+                id="name" 
+                name="name"
+                placeholder="write your text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+            <label htmlFor="password">Senha: </label>
+                <input 
+                type="password"
+                id="password"
+                name="password"
+                placeholder="write your password"
+                onChange={(e) => setPassword(e.target.value)}
+                />
             </div>
             <div>
                 <input type="submit" value="Register"/>
